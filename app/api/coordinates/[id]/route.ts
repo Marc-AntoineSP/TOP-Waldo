6e46ff41b93aa28a1fc7ab0bbe7c4fe92e723b80
Marc-AntoineSP <marcantoine.saintpierre@viacesi.fr>
2025-09-16 16:49:37 +0200

    Fix : Coordinates from targets

--- app/api/coordinates/[id]/route.ts
+++ app/api/coordinates/[id]/route.ts
@@ -10,5 +10,5 @@ export async function GET(req: Request, context: {params: Promise<{id:string}>})
             map:Number(id)
         }
     })
-    return NextResponse.json(res);
+    return NextResponse.json(res?.coordinates);
 }
